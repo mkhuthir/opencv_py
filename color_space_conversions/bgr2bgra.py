43ff9c7bcd2a42095c6c2330e18a7465a3d0136d
Muthanna Alwahash <mkhuthir@gmail.com>
2024-02-16 20:01:37 +0400

    w

--- color_space_conversions/bgr2bgra.py
+++ color_space_conversions/bgr2bgra.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 
+# Muthanna Alwahash
+# Feb 2024
+
 import numpy as np
-import cv2
+import cv2 as cv
 
 # Load BGR image
-bgr = cv2.imread("../img/butterfly.jpg", 1)
+bgr = cv.imread("../img/butterfly.jpg", 1)
 
 # Show BGR image
-cv2.imshow("BGR",bgr)
-cv2.moveWindow("BGR",0,0)
+cv.imshow("BGR",bgr)
+cv.moveWindow("BGR",0,0)
 
 print(bgr.shape)
 height,width,channels = bgr.shape
@@ -19,12 +22,12 @@ g = bgr[:,:,1]
 r = bgr[:,:,2]
 
 # create new image BGR and add G as transperancy channel
-bgra = cv2.merge((b,g,r,g))
-cv2.imwrite("../img/butterfly_bgra.png",bgra)
+bgra = cv.merge((b,g,r,g))
+cv.imwrite("../img/butterfly_bgra.png",bgra)
 
-cv2.imshow("BGRA",bgra)
-cv2.moveWindow("BGRA",width,0)
+cv.imshow("BGRA",bgra)
+cv.moveWindow("BGRA",width+10,0)
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+cv.waitKey(0)
+cv.destroyAllWindows()
 
