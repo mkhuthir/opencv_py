70e095ce201d6dfd20722b808e7eaa483cb9e4b8
mkhuthir <mkhuthir@gmail.com>
2024-02-17 19:13:43 +0400

    w

--- image_io/imread.py
+++ image_io/imread.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 
-import cv2
+import cv2 as cv
 
 # Read Image
-img = cv2.imread('../img/fruits.jpg')
+img = cv.imread('../img/fruits.jpg')
 
 # Show image
-cv2.namedWindow('image',cv2.WINDOW_NORMAL)
-cv2.imshow('image',img)
+cv.namedWindow('image',cv.WINDOW_NORMAL)
+cv.imshow('image',img)
 
 # Wait for a key
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+cv.waitKey(0)
+cv.destroyAllWindows()
 
