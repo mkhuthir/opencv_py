b68da46fbd5429e6b8030ed43ad9bb28e9bc9334
mkhuthir <mkhuthir@gmail.com>
2024-02-24 09:26:32 +0400

    w

---
+++ info/info.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# Muthanna Alwahash
+# Feb 2024
+
+import cv2 as cv
+
+print("\nPrinting info for cv module:....\n")
+
+print("Package :",cv.__package__)
+print("Name    :",cv.__name__)
+print("Doc     :",cv.__doc__)
+print("Version :",cv.__version__)
+print("Path    :",cv.__path__)
+print("Files   :",cv.__file__)
+print("\n")
+
+
+
+
